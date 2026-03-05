1a5c2a2d4756c5ae14b4622f483006ec22d301e0
cdavila58 <cdavila@att.net>
2026-03-04 18:11:47 -0600

    Update app.js

--- convolution_animated/app.js
+++ convolution_animated/app.js
@@ -222,7 +222,7 @@ function setTLabel(text) {
 function fitCanvas(canvas) {
   const dpr = window.devicePixelRatio || 1;
   const availW = window.innerWidth;
-  const availH = Math.floor(window.innerHeight * 0.80);
+  const availH = Math.floor(window.innerHeight * 0.90);
 
   const s = Math.min(availW / BASE_W, availH / BASE_H);
 
@@ -613,3 +613,4 @@ function init() {
 
 
 init();
+
